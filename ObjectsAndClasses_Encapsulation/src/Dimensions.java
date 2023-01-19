9ddf1d7500304a015cba40fa11f9ed22c0f82964
dmitrii_kobtsev <pick.pak2017@yandex.ru>
2023-01-19 13:31:50 +0300

    Homework

--- ObjectsAndClasses_Encapsulation/src/Dimensions.java
+++ ObjectsAndClasses_Encapsulation/src/Dimensions.java
@@ -1,9 +1,9 @@
 package src;
 
 public class Dimensions {
-    private final int width;
-    private final int height;
-    private final int length;
+    private int width;
+    private int height;
+    private int length;
 
     public Dimensions(int width, int height, int length) {
         this.width = width;
@@ -15,27 +15,53 @@ public class Dimensions {
         return width;
     }
 
-    public Dimensions setWidth(int width) {
-        return new Dimensions(width, height, length);
+    public void setWidth(int width) {
+        this.width = width;
     }
 
     public int getHeight() {
         return height;
     }
 
-    public Dimensions setHeight(int height) {
-        return new Dimensions(width, height, length);
+    public void setHeight(int height) {
+        this.height = height;
     }
 
     public int getLength() {
         return length;
     }
 
-    public Dimensions setLength(int length) {
-        return new Dimensions(width, height, length);
+    public void setLength(int length) {
+        this.length = length;
     }
 
     public void toPrintVolume() {
         System.out.println(width * height * length);
     }
+
+//    public int getWidth() {
+//        return width;
+//    }
+//
+//    public Dimensions setWidth(int width) {
+//        return new Dimensions(width, height, length);
+//    }
+//
+//    public int getHeight() {
+//        return height;
+//    }
+//
+//    public Dimensions setHeight(int height) {
+//        return new Dimensions(width, height, length);
+//    }
+//
+//    public int getLength() {
+//        return length;
+//    }
+//
+//    public Dimensions setLength(int length) {
+//        return new Dimensions(width, height, length);
+//    }
+
+
 }
