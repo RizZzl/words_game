05a1b5cdd9a64073af94e75a979fda08def08b58
dmitrii_kobtsev <pick.pak2017@yandex.ru>
2023-01-29 10:17:50 +0300

    git push origin homework

--- java_numbersanddates-master/src/main/java/practice/Birthdays.java
+++ java_numbersanddates-master/src/main/java/practice/Birthdays.java
@@ -2,6 +2,7 @@ package practice;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.Locale;
 
 public class Birthdays {
 
@@ -16,38 +17,16 @@ public class Birthdays {
     }
 
     public static String collectBirthdays(int year, int month, int day) {
-
-
         LocalDate now = LocalDate.now();
         LocalDate birthday = LocalDate.of(year, month, day);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern(" - dd.MM.yyyy - EE", Locale.ENGLISH);
 
-
-
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        String builder = "";
         for (int i = 0; birthday.isBefore(now) || birthday.isEqual(now); i++) {
-            int numberDayOfWeek = birthday.getDayOfWeek().getValue();
-            String dayOfWeek = "";
-
-            if (numberDayOfWeek == 1) {
-                dayOfWeek = "Mon";
-            } else if (numberDayOfWeek == 2) {
-                dayOfWeek = "Tue";
-            } else if (numberDayOfWeek == 3) {
-                dayOfWeek = "Wed";
-            } else if (numberDayOfWeek == 4) {
-                dayOfWeek = "Thu";
-            } else if (numberDayOfWeek == 5) {
-                dayOfWeek = "Fri";
-            } else if (numberDayOfWeek == 6) {
-                dayOfWeek = "Sat";
-            } else if (numberDayOfWeek == 7) {
-                dayOfWeek = "Sun";
-            }
-
-            System.out.println(i + " - " + birthday.format(formatter) + " - " + dayOfWeek);
+            builder = builder + i + birthday.format(formatter) + "\n";
             birthday = birthday.plusYears(1);
+
         }
-        
-        return "";
+        return builder;
     }
 }
