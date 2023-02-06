67beceacc49fd96e055dcbf3e49d0cc4c5e364df
dmitrii_kobtsev <pick.pak2017@yandex.ru>
2023-02-06 10:18:56 +0300

    homework №2

--- java_collectionsarray-master/src/main/java/practice/hospital/Hospital.java
+++ java_collectionsarray-master/src/main/java/practice/hospital/Hospital.java
@@ -1,27 +1,53 @@
 package practice.hospital;
 
+import java.util.Arrays;
+
 public class Hospital {
+
 
     public static float[] generatePatientsTemperatures(int patientsCount) {
         double minTemp = 32;
         double maxTemp = 40;
         int[] patients = new int[patientsCount];
-        float patientsTemp = 0;
+        float[] patientsTemp = new float[patientsCount];
 
         for (int i = 0; i < patients.length; i++) {
-            double randomPatientsTemp = ((Math.random() * Math.round((maxTemp - minTemp) + 1)) + minTemp);
-            patients[i] = (int) randomPatientsTemp;
-            patientsTemp = patientsTemp + patients[i];
+            double randomPatientsTemp = (Math.random() * Math.round(maxTemp - minTemp) + minTemp);
+            patientsTemp[i] = (float) randomPatientsTemp;
         }
-        return new float[(int) patientsTemp];
+        return patientsTemp;
     }
 
     public static String getReport(float[] temperatureData) {
+        double minHealthTemp = 36.2;
+        double maxHealthTemp = 36.9;
+
+        float[] patientsTemp = Hospital.generatePatientsTemperatures(30);
+        double average = 0;
+
+        if (patientsTemp.length > 0) {
+            double sum = 0;
+
+            for (int i = 0; i < patientsTemp.length - 1; i++) {
+                sum += patientsTemp[i];
+            }
+            average = sum / patientsTemp.length;
+        }
+        String averageTemp  = String.format("%.2f",average);
+
+
+        int patientsHealth = 0;
+        for (int i = 0; i < patientsTemp.length - 1; i++) {
+            double temp = patientsTemp[i];
+            if (minHealthTemp <= temp || maxHealthTemp >= temp) {
+                patientsHealth++;
+            }
+        }
 
         String report =
-            "Температуры пациентов: " + 1 +
-            "\nСредняя температура: " + 0 +
-            "\nКоличество здоровых: " + 0;
+            "Температуры пациентов: " + Arrays.toString(patientsTemp) +
+            "\nСредняя температура: " + averageTemp +
+            "\nКоличество здоровых: " + patientsHealth;
 
         return report;
     }
