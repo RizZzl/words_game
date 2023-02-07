2b19f49eb87d1235e5219e82c5c143c2e2e116bf
dmitrii_kobtsev <pick.pak2017@yandex.ru>
2023-02-07 17:28:36 +0300

    homework

--- java_collectionsarray-master/src/main/java/practice/hospital/Hospital.java
+++ java_collectionsarray-master/src/main/java/practice/hospital/Hospital.java
@@ -20,13 +20,13 @@ public class Hospital {
 
     public static String getReport(float[] temperatureData) {
 
-        float[] patientsTemp = Hospital.generatePatientsTemperatures(30);
+        float[] patientsTemp = temperatureData;
         double average = 0;
 
         if (patientsTemp.length > 0) {
             double sum = 0;
 
-            for (int i = 0; i < patientsTemp.length - 1; i++) {
+            for (int i = 0; i < patientsTemp.length; i++) {
                 sum += patientsTemp[i];
             }
             average = sum / patientsTemp.length;
@@ -37,13 +37,19 @@ public class Hospital {
         int patientsHealth = 0;
         for (int i = 0; i < patientsTemp.length - 1; i++) {
             float temp = patientsTemp[i];
-            if (36.2f <= temp || temp <= 36.9f) {
+            if (36.2f <= temp && temp <= 36.9f) {
                 patientsHealth++;
             }
         }
 
+        StringBuilder builder = new StringBuilder();
+        for(int i = 0; i < patientsTemp.length; i++) {
+            builder.append(patientsTemp[i]).append(" ");
+        }
+        String strPatientsTemp = String.valueOf(builder);
+
         String report =
-            "Температуры пациентов: " + Arrays.toString(patientsTemp) +
+            "Температуры пациентов: " + strPatientsTemp.trim() +
             "\nСредняя температура: " + averageTemp +
             "\nКоличество здоровых: " + patientsHealth;
 
