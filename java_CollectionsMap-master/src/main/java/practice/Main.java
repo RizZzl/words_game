6eb0747111d88fad3c6d500ce6429dfdb9ca3200
dmitrii_kobtsev <pick.pak2017@yandex.ru>
2023-02-11 15:47:40 +0300

    unfinished homework

---
+++ java_CollectionsMap-master/src/main/java/practice/Main.java
@@ -0,0 +1,33 @@
+package practice;
+import java.util.Scanner;
+
+public class Main {
+    private static PhoneBook phonesBook = new PhoneBook();
+
+    public static void main(String[] args) {
+
+        while (true) {
+            System.out.println("Введите номер, имя или команду:");
+            Scanner scanner = new Scanner(System.in);
+            String str = scanner.nextLine();
+
+            String command = str.replaceAll("[^A-Z]+", "");
+            String name = str.replaceAll("[^А-Яа-я]+", "");
+            String phone = str.replaceAll("[^0-9]+", "");
+
+            if (str.equals(name)) {
+                phonesBook.addContact(phone, name);
+                phonesBook.getContactByName(name);
+            }
+
+            if (str.equals(phone)) {
+                phonesBook.addContact(phone, name);
+                phonesBook.getContactByPhone(phone);
+            }
+
+            if (command.equals("LIST")) {
+                System.out.println(phonesBook.getAllContacts());
+            }
+        }
+    }
+}
---
+++ java_CollectionsMap-master/src/main/java/practice/PhoneBook.java
@@ -0,0 +1,70 @@
+package practice;
+
+import java.util.*;
+
+public class PhoneBook {
+    HashMap<String, String> phoneBook = new HashMap<>();
+    public void addContact(String phone, String name) {
+        // проверьте корректность формата имени и телефона
+        // (рекомедуется написать отдельные методы для проверки является строка именем/телефоном)
+        // если такой номер уже есть в списке, то перезаписать имя абонента
+        if (phone.matches("[\\d]{11}") && name.matches("[А-Яа-я]+")){
+            if (phoneBook.containsKey(phone)){
+                phoneBook.replace(phone, phoneBook.get(phone), name);
+            } else {
+                phoneBook.put(phone, name);
+            }
+        }
+    }
+
+
+    public String getContactByPhone(String phone) {
+        // формат одного контакта "Имя - Телефон"
+        // если контакт не найдены - вернуть пустую строку
+        if (phoneBook.containsKey(phone)) {
+            return phoneBook.get(phone) + " - " + phone;
+        } else {
+            return "";
+        }
+    }
+
+    public Set<String> getContactByName(String name) {
+        // формат одного контакта "Имя - Телефон"
+        // если контакт не найден - вернуть пустой TreeSet
+        TreeSet<String> setPhoneBook = new TreeSet<>();
+        HashMap<String, List<String>> multiMap = new HashMap<>();
+        ArrayList<String> twoTelephones = new ArrayList<>();
+        String telPlusName = "";
+        if (phoneBook.containsValue(name)) {
+            telPlusName = telPlusName.concat(name) + " - ";
+            for (String telephone : phoneBook.keySet()) {
+                if (phoneBook.get(telephone).equals(name)) {
+                    telPlusName = telPlusName.concat(telephone) + ", ";
+                }
+            }
+            setPhoneBook.add(telPlusName.substring(0, telPlusName.length() - 2));
+            return setPhoneBook;
+        } else {
+            return new TreeSet<>();
+        }
+    }
+
+    public Set<String> getAllContacts() {
+        // формат одного контакта "Имя - Телефон"
+        // если контактов нет в телефонной книге - вернуть пустой TreeSet
+        TreeSet<String> setPhoneBook = new TreeSet<>();
+        for (String key : phoneBook.keySet()) {
+            setPhoneBook.add(phoneBook.get(key) + " - " + key);
+        }
+        return new TreeSet<>(setPhoneBook);
+    }
+
+    // для обхода Map используйте получение пары ключ->значение Map.Entry<String,String>
+    // это поможет вам найти все ключи (key) по значению (value)
+    /*
+        for (Map.Entry<String, String> entry : map.entrySet()){
+            String key = entry.getKey(); // получения ключа
+            String value = entry.getValue(); // получения ключа
+        }
+    */
+}
