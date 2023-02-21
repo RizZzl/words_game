59634d54004e466a344e8a9bb2bd50d47bb6813b
Дмитрий Кобцев <pick.pak2017@yandex.ru>
2023-02-21 20:57:40 +0300

    Upload New File

---
+++ AbstractClasses/practice_1/Operator.java
@@ -0,0 +1,9 @@
+package practice_1;
+
+public class Operator implements Employee {
+    private final double FIX_SALARY = 25000.0;
+
+    public double getMonthSalary() {
+        return FIX_SALARY;
+    }
+}
