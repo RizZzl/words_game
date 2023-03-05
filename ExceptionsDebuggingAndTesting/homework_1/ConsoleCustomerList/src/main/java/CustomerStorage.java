67eb2298169bf8c3ef024fc2b3bb08535805c579
dmitrii_kobtsev <pick.pak2017@yandex.ru>
2023-03-05 13:50:51 +0300

    homework

--- ExceptionsDebuggingAndTesting/homework_1/ConsoleCustomerList/src/main/java/CustomerStorage.java
+++ ExceptionsDebuggingAndTesting/homework_1/ConsoleCustomerList/src/main/java/CustomerStorage.java
@@ -8,22 +8,43 @@ public class CustomerStorage {
         storage = new HashMap<>();
     }
 
-    public void addCustomer(String data) {
+    public void addCustomer(String data) throws IllegalAccessException {
         final int INDEX_NAME = 0;
         final int INDEX_SURNAME = 1;
         final int INDEX_EMAIL = 2;
         final int INDEX_PHONE = 3;
 
+        String phoneNumberRegExp = "^(\\+7|8)[0-9]{10}";
+        String emailCheckRegExp = "\\A[^@]+@([^@\\.]+\\.)+[^@\\.]+\\z";
+
         String[] components = data.split("\\s+");
+        if (components.length != 4) {
+            throw new IllegalArgumentException("Wrong format.\nCorrect format: " +
+                    "Василий Петров vasily.petrov@gmail.com +79215637722");
+        }
+        if(!components[2].matches(emailCheckRegExp)) {// проверяем почтовый ящик
+            throw new IllegalArgumentException("Wrong format e-mail address: " + components[2] +
+                    "\nCorrect format: vasily.petrov@gmail.com");
+        }
+        if(!components[3].matches(phoneNumberRegExp)) // проверяем номер телефона
+            throw new IllegalArgumentException("Wrong format phone number: "  + components[3] +
+                    "\nCorrect format: +79215637722");
+
         String name = components[INDEX_NAME] + " " + components[INDEX_SURNAME];
         storage.put(name, new Customer(name, components[INDEX_PHONE], components[INDEX_EMAIL]));
     }
 
     public void listCustomers() {
+        if(storage.size() == 0) {
+            throw new IllegalArgumentException("The list of clients is empty");
+        }
         storage.values().forEach(System.out::println);
     }
 
     public void removeCustomer(String name) {
+        if(!storage.containsKey(name)) {
+            throw new IllegalArgumentException("The specified client was not found");
+        }
         storage.remove(name);
     }
 
--- ExceptionsDebuggingAndTesting/homework_1/ConsoleCustomerList/src/main/java/Main.java
+++ ExceptionsDebuggingAndTesting/homework_1/ConsoleCustomerList/src/main/java/Main.java
@@ -1,6 +1,14 @@
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import java.util.Scanner;
 
+
+
 public class Main {
+
+    private static final Logger LOGGER = LogManager.getLogger(Main.class);
+
     private static final String ADD_COMMAND = "add Василий Петров " +
             "vasily.petrov@gmail.com +79215637722";
     private static final String COMMAND_EXAMPLES = "\t" + ADD_COMMAND + "\n" +
@@ -9,27 +17,33 @@ public class Main {
             COMMAND_EXAMPLES;
     private static final String helpText = "Command examples:\n" + COMMAND_EXAMPLES;
 
-    public static void main(String[] args) {
+
+    public static void main(String[] args) throws IllegalAccessException {
         Scanner scanner = new Scanner(System.in);
         CustomerStorage executor = new CustomerStorage();
 
         while (true) {
             String command = scanner.nextLine();
             String[] tokens = command.split("\\s+", 2);
-
-            if (tokens[0].equals("add")) {
-                executor.addCustomer(tokens[1]);
-            } else if (tokens[0].equals("list")) {
-                executor.listCustomers();
-            } else if (tokens[0].equals("remove")) {
-                executor.removeCustomer(tokens[1]);
-            } else if (tokens[0].equals("count")) {
-                System.out.println("There are " + executor.getCount() + " customers");
-            } else if (tokens[0].equals("help")) {
-                System.out.println(helpText);
-            } else {
-                System.out.println(COMMAND_ERROR);
+            try {
+                if (tokens[0].equals("add")) {
+                    executor.addCustomer(tokens[1]);
+                } else if (tokens[0].equals("list")) {
+                    executor.listCustomers();
+                } else if (tokens[0].equals("remove")) {
+                    executor.removeCustomer(tokens[1]);
+                } else if (tokens[0].equals("count")) {
+                    System.out.println("There are " + executor.getCount() + " customers");
+                } else if (tokens[0].equals("help")) {
+                    System.out.println(helpText);
+                } else {
+                    System.out.println(COMMAND_ERROR);
+                }
+            } catch (IllegalArgumentException ex) {
+                LOGGER.info(ex.getMessage());
+                System.out.println(ex.getMessage());
             }
+
         }
     }
 }
