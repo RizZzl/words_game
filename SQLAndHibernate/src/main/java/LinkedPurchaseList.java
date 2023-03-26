4a73b947c754b4fe133f2b6a8cebb3f448884f07
dmitrii_kobtsev <pick.pak2017@yandex.ru>
2023-03-26 10:29:46 +0300

    homework

---
+++ SQLAndHibernate/src/main/java/LinkedPurchaseList.java
@@ -0,0 +1,27 @@
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import java.io.Serializable;
+
+@Embeddable
+class LinkedPurchaseListKey implements Serializable {
+
+    @Column(name = "student_id")
+    private int studentId;
+
+    @Column(name = "course_id")
+    private int courseId;
+}
+
+@Entity
+public class LinkedPurchaseList {
+    @EmbeddedId
+    private LinkedPurchaseListKey id;
+
+    @Column(name = "student_id", insertable = false, updatable = false)
+    private int studentId;
+
+    @Column(name = "course_id", insertable = false, updatable = false)
+    private int courseId;
+}
