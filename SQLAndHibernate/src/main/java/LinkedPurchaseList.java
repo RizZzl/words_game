a156710ccdc7b19271b8cad6cd527969e1597c65
dmitrii_kobtsev <pick.pak2017@yandex.ru>
2023-03-26 14:03:03 +0300

    add Getter and Setter

--- SQLAndHibernate/src/main/java/LinkedPurchaseList.java
+++ SQLAndHibernate/src/main/java/LinkedPurchaseList.java
@@ -24,4 +24,30 @@ public class LinkedPurchaseList {
 
     @Column(name = "course_id", insertable = false, updatable = false)
     private int courseId;
+
+    ////////////////////////////////////////////////
+
+    public LinkedPurchaseListKey getId() {
+        return id;
+    }
+
+    public void setId(LinkedPurchaseListKey id) {
+        this.id = id;
+    }
+
+    public int getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(int studentId) {
+        this.studentId = studentId;
+    }
+
+    public int getCourseId() {
+        return courseId;
+    }
+
+    public void setCourseId(int courseId) {
+        this.courseId = courseId;
+    }
 }
