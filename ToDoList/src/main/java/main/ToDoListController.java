3a0c85fd85303abb2b99b7c725131fbf040cb5ce
dmitrii_kobtsev <pick.pak2017@yandex.ru>
2023-04-08 14:49:36 +0300

    ERROR

--- ToDoList/src/main/java/main/ToDoListController.java
+++ ToDoList/src/main/java/main/ToDoListController.java
@@ -1,33 +1,44 @@
 package main;
 
 import main.model.ToDoList;
+import main.model.ToDoListRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
-
+import java.util.Optional;
 
 @RestController
 public class ToDoListController {
+    @Autowired
+    private ToDoListRepository toDoListRepository;
+
     @GetMapping("/toDoList/")
     public List<ToDoList> list() {
-        return Storage.getAllToDoList();
+        Iterable<ToDoList> toDoListIterable = toDoListRepository.findAll();
+        ArrayList<ToDoList> toDoLists = new ArrayList<>();
+        for (ToDoList toDoList : toDoListIterable) {
+            toDoLists.add(toDoList);
+        }
+        return toDoLists;
     }
 
     @GetMapping("/toDoList/{id}")
     public ResponseEntity get(@PathVariable int id) {
-        ToDoList toDo = Storage.getToDo(id);
-        if (toDo == null) {
+        Optional<ToDoList> toDoOptional = toDoListRepository.findById(id);
+        if (!toDoOptional.isPresent()) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
         }
-        return new ResponseEntity(toDo, HttpStatus.OK);
+        return new ResponseEntity(toDoOptional.get(), HttpStatus.OK);
     }
 
     @PostMapping("/toDoList/")
     public int add(ToDoList toDo) {
-        return Storage.addToDoList(toDo);
+        ToDoList toDoList = toDoListRepository.save(toDo);
+        return toDoList.getId();
     }
 
     @PostMapping("/toDoList/{id}")
@@ -37,44 +48,43 @@ public class ToDoListController {
 
     @PutMapping("/toDoList/")
     public ResponseEntity put(List<Integer> idList, List<ToDoList> list) {
+
         for (int i = 0; i < list.size(); i++) {
             ToDoList toDoList = list.get(i);
             int id = idList.get(i);
 
-            if (Storage.getToDo(id) == null) {
+            if (!toDoListRepository.findById(id).isPresent()) {
                 return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
             }
-            Storage.change(id, toDoList);
+            toDoListRepository.save(toDoList);
         }
         return (ResponseEntity) ResponseEntity.status(HttpStatus.OK);
     }
 
     @PutMapping("/toDoList/{id}")
     public ResponseEntity change(ToDoList toDo, @PathVariable int id) {
-        ToDoList toDoList = Storage.getToDo(id);
-        if (toDoList == null) {
+        if (!toDoListRepository.findById(id).isPresent()) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
         }
-        Storage.change(id, toDoList);
+        toDoListRepository.save(toDo);
         return (ResponseEntity) ResponseEntity.status(HttpStatus.OK);
     }
 
     @DeleteMapping("/toDoList/")
     public ResponseEntity deleteToDoList() {
-        if (!Storage.getAllToDoList().isEmpty()) {
+        if (toDoListRepository.count() == 0) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
         }
-        Storage.deleteToDoList();
+        toDoListRepository.deleteAll();
         return (ResponseEntity) ResponseEntity.status(HttpStatus.OK);
     }
 
     @DeleteMapping("/toDoList/{id}")
     public ResponseEntity removeToDo(@PathVariable int id) {
-        ToDoList toDoList = Storage.getToDo(id);
-        if (toDoList == null) {
+        if (!toDoListRepository.findById(id).isPresent()) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
         }
-        Storage.removeToDo(id);
+        toDoListRepository.deleteById(id);
         return (ResponseEntity) ResponseEntity.status(HttpStatus.OK);
     }
 }
