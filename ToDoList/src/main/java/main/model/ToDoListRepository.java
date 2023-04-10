d70336ebd847c7a83b2df5a23fb8894e50f53221
dmitrii_kobtsev <pick.pak2017@yandex.ru>
2023-04-10 11:09:15 +0300

    homework

--- ToDoList/src/main/java/main/model/ToDoListRepository.java
+++ ToDoList/src/main/java/main/model/ToDoListRepository.java
@@ -3,8 +3,6 @@ package main.model;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
 public interface ToDoListRepository extends CrudRepository<ToDoList, Integer> {
 }
